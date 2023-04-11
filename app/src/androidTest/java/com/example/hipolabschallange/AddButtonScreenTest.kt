49f01ae203d6ac04faf963b0075b5f105ee914ee
Ozlem Basabakar <ozlembasabakar@gmail.com>
2023-04-11 18:51:29 +0300

    added ui test for add button.

---
+++ app/src/androidTest/java/com/example/hipolabschallange/AddButtonScreenTest.kt
@@ -0,0 +1,38 @@
+package com.example.hipolabschallange
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.hipolabschallange.designsystem.theme.AddButton
+import org.junit.Assert.assertEquals
+import org.junit.FixMethodOrder
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runners.MethodSorters
+
+fun SemanticsNodeInteraction.assertButtonColor(expectedColor: Color) {
+    val capturedName = captureToImage().colorSpace.name
+    assertEquals(expectedColor.colorSpace.name, capturedName)
+}
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class AddButtonScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun test_addButton_isVisible() {
+        composeTestRule.onNodeWithTag("AddButton").assertExists()
+    }
+
+    @Test
+    fun test_addButton_isClickable() {
+        composeTestRule.onNodeWithTag("AddButton").assertHasClickAction()
+    }
+
+    @Test
+    fun test_addButton_backgroundColor_isSuccessful() {
+        composeTestRule.onNodeWithTag("AddButton").assertButtonColor(AddButton)
+    }
+}
